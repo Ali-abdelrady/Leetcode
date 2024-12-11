e0e0fcea8033bd743d9458d1238fd984318e3e9b
ali_abdelrady <110298275+Ali-abdelrady@users.noreply.github.com>
2024-12-11 14:41:13 +0200

    Time: 627 ms (5.88%), Space: 29.2 MB (5.56%) - LeetHub

--- 2891-maximum-beauty-of-an-array-after-applying-operation/2891-maximum-beauty-of-an-array-after-applying-operation.go
+++ 2891-maximum-beauty-of-an-array-after-applying-operation/2891-maximum-beauty-of-an-array-after-applying-operation.go
@@ -1,14 +1,29 @@
+type pair struct {
+	first int 
+	second int
+} 
 func maximumBeauty(nums []int, k int) int {
-    result := 0
-	
-	sort.Ints(nums)
+    pairs := []pair{}
 
-	l := 0 
-	for r  := range nums {
-		for nums[r] - nums[l] > 2 * k {
-			l++
+	for _,num := range nums {
+		pairs = append(pairs, pair{num - k , 1})
+		pairs = append(pairs, pair{num + k + 1  , -1})
+	}
+	//sort them based of first element
+
+	sort.Slice(pairs,func(i, j int) bool {
+		if pairs[i].first == pairs[j].first {
+			return pairs[i].second < pairs[j].second
 		}
-		result = max(result,r-l+1)
+		return pairs[i].first < pairs[j].first
+	})
+    fmt.Println(pairs)
+	result := 0 
+	cnt := 0 
+
+	for _,pair := range pairs{
+		cnt += pair.second
+		result = max(result , cnt)
 	}
 	return result
 }
