6d4d5271bc72da0ac55f9a6249355b4e740415c5
ali_abdelrady <110298275+Ali-abdelrady@users.noreply.github.com>
2024-12-24 20:18:49 +0200

    Time: 660 ms (5.56%), Space: 30.8 MB (26.6%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.py
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.py
@@ -1,19 +1,25 @@
 class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
-        mp = {}
+        def binary_search(target):
+            i,j = 0,len(nums)-1
+            while i <= j :
+                mid = int(i + (j - i) / 2)
+                if nums[mid] == target:
+                    return True
+                elif nums[mid] > target:
+                    j = mid - 1
+                else:
+                    i = mid + 1
+            return  False
 
-        for num in nums :
-            if num not in mp :
-                mp[num] = True
-
+        nums.sort()
+        cnt = 1 
         res = []
-        cnt = 1
         while cnt <= len(nums):
-            if cnt not in mp :
-                res.append(cnt)    
+            if not binary_search(cnt):
+                res.append(cnt)
             cnt += 1
 
-        print(res)
         return res
 
           
