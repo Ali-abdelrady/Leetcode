1059ba5ccb3572d5275ab66b378850c027498231
ali mohamed ahmed abdelrady <110298275+Ali-abdelrady@users.noreply.github.com>
2025-04-03 16:20:01 +0200

    Time: 86 ms (49.02%), Space: 17.7 MB (82.11%) - LeetHub

--- 3154-maximum-value-of-an-ordered-triplet-i/3154-maximum-value-of-an-ordered-triplet-i.py
+++ 3154-maximum-value-of-an-ordered-triplet-i/3154-maximum-value-of-an-ordered-triplet-i.py
@@ -4,7 +4,11 @@ class Solution:
         res = 0
         for i in range(n - 2):
             for j in range(i+1,n):
+                if nums[i] < nums[j] :
+                    continue
                 for k in range(j+1,n):
+                    if nums[k] < 0 :
+                        continue
                     res = max(res , (nums[i]-nums[j]) * nums[k])
 
         return res        
